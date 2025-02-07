0889daa0d1e403924e7a29a0609a3931cee2ac58
Joonas Heikkinen <solarshadow.jh@gmail.com>
2025-02-07 13:58:29 +0200

    5886014

---
+++ index.8848e2e4.js
@@ -0,0 +1,2 @@
+Object.values(document.getElementsByClassName("logo")).forEach((t,s)=>{t.addEventListener("click",e)});function e({target:e}){e.classList.remove("logo"),setTimeout(()=>{e.classList.add("logo")},100)}
+//# sourceMappingURL=index.8848e2e4.js.map
